790f3ccde51d56bbc0e3b8acd178920bbeba80d1
nxvjot7 <nxvjot7@google.com>
2026-05-06 16:34:44 +0530

    fix formatting in Raffle.sol

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -24,7 +24,6 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.19;
 
-
 import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
 import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
 /**
